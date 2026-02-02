c674589a63848edf4cf1fd71531e648057064658
Kim-BanSeok <101537989+Kim-BanSeok@users.noreply.github.com>
2026-02-02 22:40:43 +0900

    검색등록
    검색

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,10 @@ export const metadata: Metadata = {
   description: 'PC에서 전화번호를 입력하거나 대량으로 정리하여 휴대폰 주소록에 바로 저장 가능한 vCard 파일을 생성해드립니다.',
   other: {
     'google-adsense-account': 'ca-pub-7373977880685678',
+    // Google Search Console 검증 코드
+    'google-site-verification': 'lSJTeWuV8EZQIBkHAfSRPQlK59uyaYjsnYH_DhIv2r4',
+    // 네이버 서치어드바이저 검증 코드
+    'naver-site-verification': 'bd52bdc3dcad95af57f1a72179ff4150e28deec5',
   },
 }
 
